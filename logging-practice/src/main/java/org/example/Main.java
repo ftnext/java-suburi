e91fa193aa41f0da2c3a02aacdfcff35e52d386a
ftnext <takuyafjp+develop@gmail.com>
2024-07-08 00:56:15 +0900

    [feat]: Run slf4j app (jar-with-dependencies)

--- logging-practice/src/main/java/org/example/Main.java
+++ logging-practice/src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
     public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        logger.info("Start");
         System.out.println("Hello world!");
+        logger.debug("End");
     }
 }
